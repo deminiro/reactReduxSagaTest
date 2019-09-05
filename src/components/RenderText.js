c60cb26b9850abf52191519bc0e91374d8a120aa
deminiro <43145879+deminiro@users.noreply.github.com>
2019-09-05 19:23:51 +0300

    Update RenderText.js

--- src/components/RenderText.js
+++ src/components/RenderText.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import propTypes from 'prop-types';
 
-const RenderText = ({ result }) => (
-  <p style={{ marginLeft: '30px' }}>{!Array.isArray(result) ? result : result.join(' ')}</p>
-);
+const RenderText = ({ result }) => {
+  const indexOfIt = result.indexOf('It');
+  const newResult = result;
+  if (indexOfIt !== -1) newResult[indexOfIt] = 'This';
+  return (
+    <p style={{ marginLeft: '10px' }}>{!Array.isArray(newResult) ? result : newResult.join(' ')}</p>
+  );
+};
 
 export default RenderText;
 
